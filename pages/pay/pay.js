2858899ad72619fc247447d0e7f0917348041e6f
leina <675549518@qq.com>
2019-12-10 10:05:37 +0800

    12.10更新
    12.10更新

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -40,12 +40,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+	  console.log(123132,app.CartStockApi.totalActivityPrice)
 	this.setData({ // 从购物车库绑定数据
 		thisData: app.CartStockApi.say(),
 		totalActivityPrice: app.CartStockApi.totalActivityPrice,
-		totalArticle: app.CartStockApi.totalArticle
+		totalArticle: app.CartStockApi.totalArticle,
+		initTotalPrice: app.CartStockApi.totalActivityPrice
 	})
-	$.postMask($.host_memberProfit+'findMemberProfitByMemberId',{memberId:app.globalData.wxUserInfo.id}).then((res)=>{ // 返现数据请求
+	$.postMask($.host_memberProfit+'findMemberProfitByMemberId?memberId='+app.globalData.wxUserInfo.id,{
+		memberId:app.globalData.wxUserInfo.id
+	}).then((res)=>{ // 返现数据请求
 		this.setData({
 			rebate: res.remainingProfit
 		})
@@ -57,7 +61,7 @@ Page({
 	this.setData({
 		rebateIcon: !this.data.rebateIcon
 	})
-	let totalPrice = this.data.totalActivityPrice - (this.data.rebateIcon ? this.data.rebate : 0)
+	let totalPrice = this.data.initTotalPrice - (this.data.rebateIcon ? this.data.rebate : 0)
 	if(totalPrice < 0){
 		totalPrice = 0
 	}
@@ -70,25 +74,46 @@ Page({
 	let details = []
 	for(let item of app.CartStockApi.say()){
 		details.push({
+			// categoryId: item.categoryId,
+			categoryId: 174,
 			categorylId: item.categorylId,
 			productCode: item.productCode,
 			productNumber: item.dev_custom_count
 		})
 	}
+	console.log(details)
 	$.postMask($.host_pay+'createPayByWxMini',{
-		"categoryId": app.CartStockApi.type.categoryId,
 		"details": details,
 		"memberId": app.globalData.wxUserInfo.id,
-		"openid": app.globalData.wxUserInfo.openId,
-		"price": this.data.totalActivityPrice,
+		"openid": app.globalData.wxUserInfo.appOpenId,
+		"machineCode": app.globalData.machineCode,
+		"price": app.CartStockApi.totalActivityPrice,
 		"productNums": this.data.totalArticle,
-		"uprice": this.data.totalActivityPrice
+		"realPrice": this.data.totalActivityPrice,
+		"preferentialPrice": this.data.rebate
 	}).then((res) => {
-		console.log(res)
+		console.log('确认订单',res)
+		for(let item of details){
+			app.CartStockApi.sub({productCode:item.productCode})
+		}
+		app.globalData.orderNO.push(res.orderId)
+		wx.requestPayment({
+			timeStamp: res.timeStamp,
+			nonceStr: res.nonceStr,
+			package: res.packaAge,
+			signType: res.signType,
+			paySign: res.signs,
+			success (res) {
+				console.log('支付成功',res)
+				wx.navigateTo({
+					url: `/pages/pay/finish/index`
+				})
+			},
+			fail (res) {
+				console.log('支付错误',res)
+			}
+		})
 	})
-    // wx.navigateTo({
-    //   url: `/pages/pay/finish/index`
-    // })
   },
 
   /**
